1591ef81f0cc7d8305f5a09d31a6ef9f4f7fd72b
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-06-13 23:48:37 +0530

    feat: update server configuration and add package.json for project setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,21 +4,33 @@ import { connectDatabase } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middlewares/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+        credentials: true,
+    }));
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-}))
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
 connectDatabase().then(() => {
     app.listen(port, () => {
